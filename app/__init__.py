ab804b146440f75e45bdacbab2d7c97d0be2890f
miniferretti <36267317+miniferretti@users.noreply.github.com>
2020-05-05 21:30:53 +0200

    yolo

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
- 
+
+from app import routes, models
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
