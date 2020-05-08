2909fabcfbe897d96d12b9e7bc8313ac4916d9fa
NicolasSturam <61351986+NicolasSturam@users.noreply.github.com>
2020-05-08 14:05:46 +0200

    Test hello world

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,15 @@ from app import app
 
 
 @app.route('/')
-"""
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('/index'))
-    return render_template('login.html', title='Sign In', form=form)
-"""
+
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    form = LoginForm()
+#    if form.validate_on_submit():
+#        flash('Login requested for user {}, remember_me={}'.format(
+#            form.username.data, form.remember_me.data))
+#        return redirect(url_for('/index'))
+#    return render_template('login.html', title='Sign In', form=form) 
 
 @app.route('/index')
 def index():
