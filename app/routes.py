1b4565494103c7458fa4b3c3bf789e40257616da
NicolasSturam <61351986+NicolasSturam@users.noreply.github.com>
2020-05-08 18:36:26 +0200

    chap 3

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,19 @@
 
 from app import app
 from flask import render_template, flash, redirect, url_for
-#from app.forms import LoginForm
+from app.forms import LoginForm
 
 
 @app.route('/')
 
-#@app.route('/login', methods=['GET', 'POST'])
-#def login():
-#    form = LoginForm()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
 #    if form.validate_on_submit():
 #        flash('Login requested for user {}, remember_me={}'.format(
 #            form.username.data, form.remember_me.data))
 #        return redirect(url_for('/index'))
-#    return render_template('login.html', title='Sign In', form=form) 
+    return render_template('login.html', title='Sign In', form=form) 
 
 @app.route('/index')
 def index():
@@ -21,7 +21,7 @@ def index():
     users = [{'username': 'Nicolas'}, {'username': 'Donatien'}, {
         'username': 'Éléonore'}, {'username': 'Aurèle'}, {'username': 'Matteo'}]
     #return render_template('index2.html', title='Home', users=users)
-    #return render_template('index2.html')
-    return render_template('base.html')
+    return render_template('index.html')
+    #return render_template('base.html')
 
  
