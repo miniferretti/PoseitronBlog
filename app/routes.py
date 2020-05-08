0af1a6bd122414abaea2a3a9e4d76a2f307939e8
NicolasSturam <61351986+NicolasSturam@users.noreply.github.com>
2020-05-08 15:16:53 +0200

    merde

--- app/routes.py
+++ app/routes.py
@@ -20,6 +20,6 @@ def index():
     user = {'username' : 'Nicolas'}
     #user = [{'username': 'Nicolas'}, {'username': 'Donatien'}, {
     #    'username': 'Éléonore'}, {'username': 'Aurèle'}, {'username': 'Matteo'}]
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index2.html', title='Home', user=user)
 
  
