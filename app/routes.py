0c39f2d982477654d996e70031540f64d9460355
NicolasSturam <61351986+NicolasSturam@users.noreply.github.com>
2020-05-08 19:34:31 +0200

    Bug de merde

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect(url_for('/index'))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form) 
 
 @app.route('/index')
