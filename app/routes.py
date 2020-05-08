b61ab679e5a99486a0020c676c254d6100ddd806
NicolasSturam <61351986+NicolasSturam@users.noreply.github.com>
2020-05-08 18:54:50 +0200

    chap 3

--- app/routes.py
+++ app/routes.py
@@ -9,10 +9,10 @@ from app.forms import LoginForm
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-#    if form.validate_on_submit():
-#        flash('Login requested for user {}, remember_me={}'.format(
-#            form.username.data, form.remember_me.data))
-#        return redirect(url_for('/index'))
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('/index'))
     return render_template('login.html', title='Sign In', form=form) 
 
 @app.route('/index')
