38d734f34d9e687a2e8b41dd586d56c2b683711d
NicolasSturam <61351986+NicolasSturam@users.noreply.github.com>
2020-05-08 18:15:20 +0200

    0

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 
 from app import app
-from flask import render_template, flash, redirect, url_for
+#from flask import render_template, flash, redirect, url_for
 #from app.forms import LoginForm
 
 
