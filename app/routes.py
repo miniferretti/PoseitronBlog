566b659b7df9ca7629790bca89560ef117ebcc11
NicolasSturam <61351986+NicolasSturam@users.noreply.github.com>
2020-05-11 22:38:03 +0200

    test

--- app/routes.py
+++ app/routes.py
@@ -161,7 +161,7 @@ def robotPresentation():
 @app.route('/_robotData', methods =['GET'])
 @login_required
 def robotData():
-    return jsonify(resultx=random.randint(-1, 1), resulty=random.randint(-1, 1))
+    return jsonify(resultx=random.randint(0, 10), resulty=random.randint(0, 10))
 
 @app.route('/graphiques')
 @login_required
